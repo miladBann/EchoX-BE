a0efc315c828c31b873b497fbb656b31325708c9
milad <miladbannourah@outlook.com>
2024-10-06 18:15:40 +0300

    created the service and controller for searching for a song

--- server/src/search/search.service.ts
+++ server/src/search/search.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+    async SearchSongs(query: string): Promise<any> {
+        try{
+            const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                  part: 'snippet',
+                  maxResults: 10,
+                  q: query,
+                  type: 'video',
+                  key: 'AIzaSyA__t6xgZ_o0T5LM_NFEnJh4DIi-6XmLog',
+                },
+            });
+            return response.data;
+
+        }catch(error){
+            console.error("Error fetching song from youtube", error);
+            throw new HttpException("Error fetching youtube data", HttpStatus.NO_CONTENT)
+        }
+    }
+}
