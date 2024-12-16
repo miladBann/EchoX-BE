95c125a2a37e49a3290b6dc37ef43fa6caf4d3ff
miladBann <miladbannourah@outlook.com>
2024-12-16 02:02:32 +0200

    added the playlist module logic for the service and controller folder that fetch the playlists that the user already has in his youtube account

--- server/src/play-list/play-list.controller.ts
+++ server/src/play-list/play-list.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query,Res, Headers } from '@nestjs/common';
+import { PlayListService } from './play-list.service';
+import { Response } from 'express';
 
 @Controller('play-list')
-export class PlayListController {}
+export class PlayListController {
+    constructor(private readonly playlistService: PlayListService){}
+
+    private readonly userToken = `ya29.a0ARW5m77NngstThTODQHKopYtC8MaVyinP5BOCW0bEy35JcS-JwKko8fsdxCptX7OJg2eSMBQZy4QYvXFS1VUaTYgdK9LSfknpf0cTKn4BMoPhIlsga9c-mZ2RmaeR_wvqu8VGCirGVfTw2BuIxZ5jF5mDItBxWHH2p200enxaCgYKAX4SARISFQHGX2Mi09ziRTRd1OEMUFw_8e7b6Q0175`
+
+    @Get("")
+    async getPlaylists(@Res() res: Response): Promise<void> {
+        try {
+            const data = await this.playlistService.getUserPlaylists(this.userToken);
+            res.json(data);
+        } catch (error) {
+            console.log(`error: ${error}`);
+        }
+    }
+}
--- server/src/play-list/play-list.service.ts
+++ server/src/play-list/play-list.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class PlayListService {}
+export class PlayListService {
+
+    async getUserPlaylists(userToken: string): Promise<any> {
+        try {
+            const response = await axios.get("https://www.googleapis.com/youtube/v3/playlists", {
+                params: {
+                    part: "snippet",
+                    mine: true,
+                },
+                headers: {
+                    Authorization: `Bearer ${userToken}`
+                }
+                
+            })
+            return response.data;
+        } catch (error){
+            console.log("error:" + error);
+            throw new HttpException("Error fetching YouTube data", HttpStatus.NO_CONTENT);
+        }
+    }
+}
